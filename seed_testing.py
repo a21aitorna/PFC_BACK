639bfd7be75e02b4a402f9f6ef580ab0f5bd85d2
a21aitorna1 <a21aitorna@iessanclemente.net>
2025-12-07 19:13:57 +0100

    Usuario para modificar contraseña

--- seed_testing.py
+++ seed_testing.py
@@ -53,3 +53,20 @@ def seed_upload_books_user():
             answer = generate_password_hash("UploadBooks")
         )
         save_user(user)
+        
+def seed_modify_password_user():
+    """Crea un usuario para modificar la contraseña"""
+    existing_user = get_user_by_username("ModifyPassword")
+    if not existing_user:
+        user = Persona(
+            id_role=2,
+            name="ModifyPassword",
+            surname="User",
+            username = "ModifyPassword",
+            password = generate_password_hash("TestUser123.."),
+            born_date=date(1999,9,3),
+            library_name="ModifyPassword",
+            security_question="ModifyPassword",
+            answer = generate_password_hash("ModifyPassword")
+        )
+        save_user(user)        
